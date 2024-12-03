0381f655c0c555ce4c19b3d22ae72d6f328ee886
bossadapt <devinphillips20@gmail.com>
2024-12-02 18:17:03 -0600

    fixed frontend

--- ant-colony-frontend/next.config.ts
+++ ant-colony-frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output:"export",
+  //TODO: put this back before putting it ups
   basePath:"/antsim"
 };
 
--- src/main/java/org/bossadapt/ant_colony_sim/controller/SimulationController.java
+++ src/main/java/org/bossadapt/ant_colony_sim/controller/SimulationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -17,55 +18,50 @@ import jakarta.servlet.http.HttpSession;
 @EntityScan
 @RestController
 @SessionScope
-@CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
+@CrossOrigin(
+    origins = {"https://bossadapt.org"}, 
+    methods = {RequestMethod.GET,RequestMethod.OPTIONS,RequestMethod.POST},
+    allowCredentials = "true")
 public class SimulationController {
 
     SimulationController(){
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/normalSetup")
     public SimulationResponse normalSetup(HttpSession session) {
         Colony newColony = new Colony(true,1,50,10,4,0);
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/queenTest")
     public SimulationResponse queenTest(HttpSession session) {
         Colony newColony = new Colony(true,0,0,0,0,0);
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/scoutTest")
     public SimulationResponse scoutTest(HttpSession session) {
         Colony newColony = new Colony(false,0,0,0,1,0);
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/foragerTest")
     public SimulationResponse foragerTest(HttpSession session) {
         Colony newColony = new Colony(false,5,1,0,0,0);
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/soldierTest")
     public SimulationResponse soldierTest(HttpSession session) {
         Colony newColony =  new Colony(false,5,0,1,0,1);
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @PostMapping("/custom")
     public SimulationResponse soldierTest(HttpSession session,@RequestBody CustomSetupRequest custom) {
         Colony newColony =  new Colony(custom.isHatchingActive(),custom.getCustomRadius(),custom.getForagerCount(),custom.getSoldierCount(),custom.getScoutCount(),custom.getBalaCount());
         session.setAttribute("currentColony", newColony);
         return newColony.generateSimulationResponse();
     }
-    //would call it on repeat from front end and call again after displaying the last step
-    @CrossOrigin(origins = {"http://localhost:3000", "https://bossadapt.org"},allowCredentials = "true")
     @GetMapping("/step")
     public SimulationResponse step(HttpSession session) {
         Colony currentColony = (Colony) session.getAttribute("currentColony");
